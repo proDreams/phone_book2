eccc8c32a46d5c02b937a13c4528d987b9ce23dd
proDream <sushkoos@gmail.com>
2022-11-18 22:53:24 +0400

    clean

--- model.py
+++ model.py
@@ -1,16 +1,17 @@
 def get_db():
-
     with open('file.csv', 'r', encoding='utf-8') as file:
         result = []
         for i in file:
             result.append(i.rstrip().split(','))
     return result
 
+
 def add_student(contact):
     with open('file.csv', 'a', encoding='utf-8') as file:
         line = ','.join(contact)
         file.write(line)
         file.write('\n')
+
 
 def remove_student(id):
     full_db = get_db()
@@ -23,4 +24,3 @@ def remove_student(id):
         for i in new_db:
             file.write(','.join(i))
             file.write('\n')
-
