8bf46b694e0308455f2e672f2cdfd2dfc8cf62b7
proDream <sushkoos@gmail.com>
2022-11-18 21:54:23 +0400

    initial

--- model.py
+++ model.py
@@ -1,5 +1,7 @@
 def get_phonebook():
 
     with open('file.csv', 'r', encoding='utf-8') as file:
-        result = file.read().split(',')
+        result = []
+        for i in file:
+            result.append(file.readline().split(','))
     return result
