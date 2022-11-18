8412d3b974d13f21d4591a48c0c153a5c59896fb
ANDREY LEBEDEV <lebedev88@gmail.com>
2022-11-18 21:29:03 +0300

    model updated

--- model.py
+++ model.py
@@ -1,7 +1,13 @@
-def get_phonebook():
+def get_db():
 
     with open('file.csv', 'r', encoding='utf-8') as file:
         result = []
         for i in file:
             result.append(file.readline().split(','))
     return result
+
+def add_student(contact):
+    with open('file.csv', 'a', encoding='utf-8') as file:
+        line = ','.join(contact)
+        file.write(line)
+        file.write('\n')
