dc4cd3fec8fd7558cfb121c11f59c5b0612cb583
Vladimir Mikhailov <vladimir@mikhailov.in>
2022-11-19 23:25:29 +0300

    fix: find record logic

--- controller.py
+++ controller.py
@@ -25,13 +25,20 @@ def run():
                     user_interface.fields_menu('поиска')
                     search_choice = input('-: ').lower()
                     query = user_inputs.get_random_input('запрос')
-                    records = model.search_record(search_choice, query, FILEPATH)
+                    records = model.search_record(
+                        search_choice, query, FILEPATH)
                     if records:
-                        user_interface.print_message('Найдены следующие записи:')
+                        user_interface.print_message(
+                            'Найдены следующие записи:')
                         user_interface.show_database(records)
-                        find_id = user_inputs.get_random_input('id нужной записи')
+                        if len(records) > 1:
+                            find_id = user_inputs.get_random_input(
+                                'id нужной записи')
+                        else:
+                            find_id = records[0][0]
                         if not model.check_id(find_id, FILEPATH):
-                            user_interface.print_message(f'Запись c id "{find_id}" отсутствует')
+                            user_interface.print_message(
+                                f'Запись c id "{find_id}" отсутствует')
                         else:
                             inner_menu = True
                             while inner_menu:
@@ -39,24 +46,32 @@ def run():
                                 change_choice = input('-: ').lower()
                                 match change_choice:
                                     case '1':
-                                        user_interface.fields_menu('изменения', 1)
+                                        user_interface.fields_menu(
+                                            'изменения', 1)
                                         field_choice = input('-: ').lower()
-                                        new_value = user_inputs.get_random_input(f'новое значение поля {field_choice}')
-                                        updated_record = model.get_updates(find_id, field_choice, new_value, FILEPATH)
-                                        user_interface.show_record(updated_record)
+                                        new_value = user_inputs.get_random_input(
+                                            f'новое значение поля {field_choice}')
+                                        updated_record = model.get_updates(
+                                            find_id, field_choice, new_value, FILEPATH)
+                                        user_interface.show_record(
+                                            updated_record)
                                         if user_inputs.confirm_choice('Внести эти изменения?'):
-                                            model.change_field(find_id, field_choice, new_value, FILEPATH)
+                                            model.change_field(
+                                                find_id, field_choice, new_value, FILEPATH)
                                         inner_menu = False
                                     case '2':
-                                        record = model.search_record('1', find_id, FILEPATH, compliance=True)
+                                        record = model.search_record(
+                                            '1', find_id, FILEPATH, compliance=True)
                                         user_interface.show_record(*record)
                                         if user_inputs.confirm_choice('Вы действительно хотите удалить данную запись?'):
-                                            model.remove_student(find_id, FILEPATH)
+                                            model.remove_student(
+                                                find_id, FILEPATH)
                                         inner_menu = False
                                     case '0':
                                         inner_menu = False
                     else:
-                        user_interface.print_message(f'Записи по запросу "{query}" отсутствуют')
+                        user_interface.print_message(
+                            f'Записи по запросу "{query}" отсутствуют')
                 case '0':
                     running = False
 
