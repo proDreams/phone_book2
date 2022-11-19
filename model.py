382c63c4f04d840409d6c49ebb7133664689fb1e
Ivan Ashikhmin <sushkoos@gmail.com>
2022-11-19 16:08:35 +0400

    Add files via upload

--- model.py
+++ model.py
@@ -1,26 +1,71 @@
-def get_db():
-    with open('file.csv', 'r', encoding='utf-8') as file:
-        result = []
-        for i in file:
-            result.append(i.rstrip().split(','))
-    return result
+import sqlite3 as sl
+from os import path
+import user_interface
+import user_inputs
 
 
-def add_student(contact):
-    with open('file.csv', 'a', encoding='utf-8') as file:
-        line = ','.join(contact)
-        file.write(line)
-        file.write('\n')
+def create_db(file_name):
+    db = sl.connect(f'{file_name}.db')
+    db.execute('CREATE TABLE IF NOT EXISTS {}('
+               'student_id INTEGER PRIMARY KEY, '
+               'name TEXT NOT NULL, '
+               'patronym TEXT NOT NULL, '
+               'surname TEXT NOT NULL, '
+               'birthdate TEXT NOT NULL, '
+               'phone TEXT NOT NULL, '
+               'grade TEXT NOT NULL)'.format('students'))
+    db.commit()
+    db.close()
 
 
-def remove_student(id):
-    full_db = get_db()
-    new_db = []
-    for row in full_db:
-        if row[0] != id:
-            new_db.append(row)
+def get_db(file_name):
+    db = sl.connect(f'{file_name}.db')
+    cur = db.cursor()
+    cur.execute("SELECT * FROM students")
+    rows = cur.fetchall()
+    db.close()
+    return rows
 
-    with open('file.csv', 'w', encoding='utf-8') as file:
-        for i in new_db:
-            file.write(','.join(i))
-            file.write('\n')
+
+def add_student(contact, file_name):
+    create_db(file_name)
+    db = sl.connect(f'{file_name}.db')
+    cur = db.cursor()
+    cur.execute("INSERT INTO students VALUES(NULL, ?, ?, ?, ?, ?, ?)", tuple(value for value in contact))
+    db.commit()
+    db.close()
+
+
+def remove_student(s_id, file_name):
+    db = sl.connect(f'{file_name}.db')
+    cur = db.cursor()
+    cur.execute(f"DELETE FROM students WHERE student_id={s_id}")
+    db.commit()
+    db.close()
+
+
+def check_file_exist(file_name):
+    """
+    Проверяет наличие файла по указанному пути
+    """
+    if not path.exists(f'{file_name}.db'):
+        if user_inputs.ask_fill_input(0, file_name):
+            create_db(file_name)
+            return True
+        else:
+            return False
+    else:
+        return True
+
+
+def check_table_exist(file_name):
+    db = sl.connect(f'{file_name}.db')
+    cur = db.cursor()
+    cur.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='students'; ")
+    check = cur.fetchall()
+    db.close()
+    if not check:
+        user_interface.print_errors(1)
+        create_db(file_name)
+        user_interface.print_notifications(0)
+    return True
